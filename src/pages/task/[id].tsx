b19506785d4a7c48080607855a26653a0a626d83
gabriel <gabrielvqueiroz@gmail.com>
2025-04-05 12:01:23 -0400

    tela de tarefas

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,11 +1,74 @@
 import Head from "next/head";
 import styles from './styles.module.css';
 import { GetServerSideProps } from 'next';
-import { getSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { db } from "@/services/firebaseConnection";
-import { doc, collection, query, getDoc  } from "firebase/firestore";
+import { doc, collection, query, getDoc, addDoc, where, getDocs  } from "firebase/firestore";
+import Textarea from "@/components/textarea/intex";
+import { useState, ChangeEvent, FormEvent } from "react";
+import { FaTrash } from "react-icons/fa";
 
-export default function Task(){
+interface TaskProps {
+  item : {
+    tarefa: string,
+    pulic: boolean,
+    created: string,
+    user: string,
+    id: string,
+  },
+  allComments: CommentProps[],
+}
+
+interface CommentProps{
+  id:string;
+  comment:string;
+  taskId: string;
+  user: string;
+  name: string;
+}
+
+export default function Task({item, allComments}:TaskProps){
+
+  const {data: session } = useSession();
+  const [input, setInput] = useState('');
+  const [comments, setComments] = useState<CommentProps[]>(allComments || []);
+
+  async function handleComent(e: FormEvent) {
+    e.preventDefault();
+
+    if(input === '')
+      return;
+
+    if((!session?.user?.email) || (!session?.user?.name))
+      return;
+
+    try {
+      const docRef = await addDoc(collection(db, 'comments'), {
+        comment: input.trim(),
+        created: new Date(),
+        user: session?.user?.email.trim(),
+        name: session?.user?.name.trim(),
+        taskId: item?.id,
+      });
+      
+      const data = {
+        id: docRef.id,
+        comment: input,
+        user: session?.user?.email.trim(),
+        name: session?.user?.name.trim(),
+        taskId: item?.id,
+      }
+
+      setInput('');
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleDelete(id:string) {
+    
+  }
 
   return(
     <div className={styles.container}>
@@ -15,7 +78,49 @@ export default function Task(){
 
       <main className={styles.main}>
         <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>
+            {item.tarefa}
+          </p>
+        </article>
       </main>
+
+      <section className={styles.commentsCOntainer}>
+        <h2>Deixar comentários</h2>
+        <form onSubmit={handleComent}>
+          <Textarea 
+            placeholder="digite seu comentário" 
+            value={input} 
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>{
+              setInput(e.target.value)
+            }}
+          />
+          <button className={styles.button} disabled={!session?.user}>Comentar</button>
+        </form>
+
+        <section>
+          <h2>Todos Comentarios</h2>
+          {comments.length === 0 &&( <span>nenhum comentario foi encontrado</span> )}
+
+          {comments.map((item)=>(
+            <article key={item.id} className={styles.comment}>
+              <div className={styles.headComment}>
+                <label className={styles.commentsLabel}>{item.name}</label>
+
+                {(item.user === session?.user?.email ) && (
+                  <button className={styles.buttonTrash} onClick={()=>{handleDelete(item.id)}}>
+                    <FaTrash size={18} color="#ea3140"/>
+                  </button>
+                )}
+                
+              </div>
+              <p>{item.comment}</p>
+            </article> 
+          ))}
+        </section>
+      </section>
+
+      
 
     </div>
   )
@@ -24,8 +129,25 @@ export default function Task(){
 export const getServerSideProps: GetServerSideProps = async({params}) =>{
 
   const id = params?.id as string;
-  const docRef = doc(db, 'tarefas', id);
 
+  //comentarios
+  const q = query(collection(db, 'comments'), where('taskId', '==', id));
+  const snapshotComments = await getDocs(q);
+
+  let allComments :CommentProps[] = [];
+
+  snapshotComments.forEach((doc)=>{
+    allComments.push({
+      id: doc.id.trim(),
+      comment: doc.data().comment.trim(),
+      taskId:doc.data().taskId.trim(),
+      user:doc.data().user.trim(),
+      name:doc.data().name.trim(),
+    })
+  });
+
+  //tarefa
+  const docRef = doc(db, 'tarefas', id);
   const snapshot = await getDoc(docRef);
 
   //se nao achou
@@ -50,18 +172,19 @@ export const getServerSideProps: GetServerSideProps = async({params}) =>{
 
   const milesseconds = snapshot.data()?.created?.seconds * 1000;
   const task = {
-    tarefa: snapshot.data()?.tarefa.trim(),
-    pulic: snapshot.data()?.public,
+    tarefa: snapshot.data()?.tarefa.trim() as string,
+    pulic: snapshot.data()?.public as boolean,
     created: new Date(milesseconds).toLocaleDateString(),
-    user: snapshot.data()?.user.trim(),
-    id: id.trim(),
+    user: snapshot.data()?.user.trim() as string,
+    id: id.trim() as string,
   }
 
-  console.log(task);
+  // console.log(task);
 
   return{
     props:{
-      // task: task,
+      item: task,
+      allComments: allComments,
     }
   }
 };
--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,13 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXMRrro28mEMqL6jM5syApusxY3U362RA",
-  authDomain: "tarefasudemy.firebaseapp.com",
-  projectId: "tarefasudemy",
-  storageBucket: "tarefasudemy.firebasestorage.app",
-  messagingSenderId: "95768179367",
-  appId: "1:95768179367:web:eebe5409bb3500209c6182",
-  measurementId: "G-6DT3GY17LS"
+  
+
+ apiKey: "AIzaSyBXMRrro28mEMqL6jM5syApusxY3U362RA", 
+ authDomain: "tarefasudemy.firebaseapp.com",
+ projectId: "tarefasudemy",
+ storageBucket: "tarefasudemy.firebasestorage.app",
+ messagingSenderId: "95768179367",
+ appId: "1:95768179367:web:eebe5409bb3500209c6182",
+ measurementId: "G-6DT3GY17LS",
+
 };
 
 // Initialize Firebase
